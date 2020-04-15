9c203d90d6de7ab7fb70c8a7aaa41f94be8a3fa8
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-04-14 22:14:35 -0400

    complete queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,12 @@
+-- Multi-Table Query Practice
+
+-- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+
+-- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+-- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+
+-- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
  -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
@@ -24,7 +33,7 @@ From OrderDetail as o
 JOIN Product as p
     ON o.ProductId = p.Id
 WHERE OrderId = 10251
-ORDER BY p.ProductName
+ORDER BY p.ProductName;
 
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
