12cab007b714a17e290d6bb1c67b7026b54b306f
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-04-14 23:32:46 -0400

    complete mvp

---
+++ schemes/scheme-model.js
@@ -0,0 +1,43 @@
+const db = require('../data/db-config.js');
+
+function find() {
+	return db('schemes');
+}
+
+function findById(id) {
+	return db('schemes').where({ id }).first();
+}
+
+function findSteps(id) {
+	return db('steps as t')
+		.join('schemes as s', { 't.scheme_id': 's.id' })
+		.where({ 't.scheme_id': id })
+		.select('t.id', 't.step_number', 's.scheme_name', 't.instructions')
+		.orderBy('t.step_number');
+}
+
+function add(data) {
+	return db('schemes').insert(data);
+}
+
+function addStep(data) {
+	return db('steps').insert(data);
+}
+
+function update(data, id) {
+	return db('schemes').where({ id }).update(data);
+}
+
+function remove(id) {
+	return db('schemes').where({ id }).del();
+}
+
+module.exports = {
+	find,
+	findById,
+	findSteps,
+	add,
+	addStep,
+	update,
+	remove,
+};
